72ad47741fd6e7cb905eb416220408e88136ecd8
Jokskuyy <160201837+Jokskuyy@users.noreply.github.com>
2024-06-20 17:36:28 +0700

    sip

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // session dimulai
-session_destroy(); // hapus session
-header("location: index.php"); // redirect ke halaman index.php
-exit(); // pastikan script berhenti setelah redirect
-?>
